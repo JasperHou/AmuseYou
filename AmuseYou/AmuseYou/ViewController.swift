4c07463bf5a44650009c750ae496ea708e71a830
Jasper Hou <jasper.hou@bmw.com>
2016-08-31 22:35:19 +0800

    init for amuse project

---
+++ AmuseYou/AmuseYou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AmuseYou
+//
+//  Created by Hou Jasper, EC-17 on 8/31/16.
+//  Copyright © 2016 amusechina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
